8bcfe62f679e750e7a2bc4fa8007cd14c09504ed
pratyush giri <gppratyush28@gmail.com>
2020-08-06 00:09:02 +0530

    Updates

---
+++ precache-manifest.0b6575821e7e3f177491506413322130.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a71b8988ad4e8c86ba849f0d89b3735",
+    "url": "/reactlive/index.html"
+  },
+  {
+    "revision": "5457ccfcb3b7d87e5476",
+    "url": "/reactlive/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "2c8bcf5bbb316da14280",
+    "url": "/reactlive/static/css/main.c4389ea3.chunk.css"
+  },
+  {
+    "revision": "5457ccfcb3b7d87e5476",
+    "url": "/reactlive/static/js/2.b8a82c80.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactlive/static/js/2.b8a82c80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c8bcf5bbb316da14280",
+    "url": "/reactlive/static/js/main.c367cbb9.chunk.js"
+  },
+  {
+    "revision": "95013b9189931895e93e",
+    "url": "/reactlive/static/js/runtime-main.a2094eba.js"
+  },
+  {
+    "revision": "86f79f7ebcf307326ddda9be7a307371",
+    "url": "/reactlive/static/media/and.86f79f7e.jpg"
+  },
+  {
+    "revision": "c3346017860f0e8f70d008eeea9d4c77",
+    "url": "/reactlive/static/media/back.c3346017.png"
+  },
+  {
+    "revision": "8f328369ab775ab1f6100a780128edc0",
+    "url": "/reactlive/static/media/ecommerce2.8f328369.png"
+  },
+  {
+    "revision": "74ecf70dc76d94ed16bb95ab819aea12",
+    "url": "/reactlive/static/media/ios.74ecf70d.png"
+  },
+  {
+    "revision": "2f3b956244cb19589139852b3edc9426",
+    "url": "/reactlive/static/media/logo.2f3b9562.png"
+  },
+  {
+    "revision": "6133bc44e8a451901da70936c455cb7b",
+    "url": "/reactlive/static/media/new.6133bc44.png"
+  },
+  {
+    "revision": "d6dde1d748aa51c4014394167c9d9a0d",
+    "url": "/reactlive/static/media/soc.d6dde1d7.png"
+  },
+  {
+    "revision": "bfbe7ee68500ea229d2c5bbd58fc4d18",
+    "url": "/reactlive/static/media/wbe.bfbe7ee6.png"
+  },
+  {
+    "revision": "fe1c0df6aedbc1dd419ee57137e8ed4b",
+    "url": "/reactlive/static/media/webs.fe1c0df6.png"
+  }
+]);
